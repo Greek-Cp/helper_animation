9f55d8e0f7f862dafd6c1804ea4c7de22dfc5812
Yulia Gita <82995911+Greek-Cp@users.noreply.github.com>
2025-04-10 15:21:12 +0700

    update v2

--- lib/helper_animation.dart
+++ lib/helper_animation.dart
@@ -1,7 +1,4 @@
 library helper_animation;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
-}
+export 'constants/enums.dart';
+export 'widgets/effect_animation.dart';
--- lib/helper_animation_library.dart
+++
@@ -1,2 +0,0 @@
-export 'constants/enums.dart';
-export 'widgets/effect_animation.dart';
